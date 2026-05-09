d002f218c0d6953455e591a1b79bb51ed973161a
n0ta <teroro1226@outlook.jp>
2026-05-09 21:03:22 +0900

    feat: Add initial flake.nix for Cisco SNMP monitoring environment.

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Cisco SNMP monitoring management environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin"; # Apple Silicon Mac
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.ansible
+          pkgs.net-snmp
+          pkgs.docker
+        ];
+      };
+    };
+}
